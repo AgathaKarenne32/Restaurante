b85cb77c83d9013c6f79af0a8a72afd6c60b66d6
AgathaKarenne32 <142344001+AgathaKarenne32@users.noreply.github.com>
2024-09-12 19:45:28 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.restaurante.sabor.gourmet;
+
+/**
+ *
+ * @author Amanda
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Cliente {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Cliente(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+    
+    // Getters para os atributos
+    public String getNome() {
+        return nome;
+    }
+
+    public String endereco(){
+        return endereco;
+    }
+
+    public String telefone(){
+        return telefone;
+    }
+    // Método para registrar o cliente em uma lista fornecida
+    public void registrarCliente(List<Cliente> listaClientes) {
+        listaClientes.add(this);
+        System.out.println("Cliente registrado com sucesso.");
+    }
+
+    public void visualizarCliente() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Endereço: " + endereco);
+        System.out.println("Telefone: " + telefone);
+    }
+}
---
+++ ItemMenu.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.restaurante.sabor.gourmet;
+
+/**
+ *
+ * @author Amanda
+ */
+
+public class ItemMenu {
+    private String nome;
+    private String categoria;
+    private double preco;
+
+    public ItemMenu(String nome, String categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+    
+    // Getters para os atributos
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+    public String getDetalhesItem() {
+        return "Nome: " + nome + ", Categoria: " + categoria + ", Preço: " + preco;
+    }
+
+    public double calcularPreco() {
+        return preco;
+    }
+}
